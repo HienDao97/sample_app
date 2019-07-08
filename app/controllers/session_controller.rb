484f5583b872c327adf98e164e4b82d3d870ca98
hiendt-0025 <dao.thi.hien@sun-asterisk.com>
2019-07-08 16:35:30 +0700

    chapter9

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,10 +2,18 @@ class SessionController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
+    user = User.find_by email: downcase_param_email
+    session = params[:session]
 
-    if user&.authenticate params[:session][:password]
+    if user&.authenticate session[:password]
       log_in user
+
+      if session[:remember_me] == Settings.remember_me_value
+        remember(user)
+      else
+        forget(user)
+      end
+
       flash[:success] = t ".success"
       redirect_to user
     else
@@ -15,7 +23,13 @@ class SessionController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def downcase_param_email
+    params[:session][:email].downcase
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,4 @@
 module UsersHelper
-  # Return the Gravatar for the given User.
   def gravatar_for user, size: Settings.size_image
     gravatar_id = Digest::MD5.hexdigest user.email.downcase
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
